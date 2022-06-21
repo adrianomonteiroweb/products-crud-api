e80e773d32f6c053f9a375635e13dbf276dc32bf
Adriano monteiro <adrianomonteiroweb@gmail.com>
2022-06-21 14:07:36 -0300

    NEW: db settings with ORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Product } from './product.model';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'adriano',
+      password: '12345678',
+      database: 'library',
+      autoLoadModels: true, // Reconhecer modelos do projeto
+      synchronize: true, // importa módulo automaticamente
+    }),
+    SequelizeModule.forFeature([Product]), // informa o model
+  ],
   controllers: [AppController, ProductsController],
   providers: [AppService, ProductsService],
 })
