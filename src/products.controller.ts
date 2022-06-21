d01e0ff63f1dfb7e4743980ae8b9186ae6143ad7
Adriano monteiro <adrianomonteiroweb@gmail.com>
2022-06-21 09:33:37 -0300

    NEW: list product by id method.

--- src/products.controller.ts
+++ src/products.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
   @Get()
   getAll(): string {
     return 'All products list';
+  }
+
+  @Get(':id')
+  getOne(@Param() params): string {
+    return `Product ${params.id}`;
   }
 }
