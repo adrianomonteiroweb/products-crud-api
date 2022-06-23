503c06d7e897331e7a0878c2829592ee994e734d
Adriano monteiro <adrianomonteiroweb@gmail.com>
2022-06-23 09:29:35 -0300

    FIX: settings in products files.

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { Product } from './product.model';
 
-@Injectable()
+@Injectable() // injeção de dependência
 export class ProductsService {
   constructor(
     @InjectModel(Product)
